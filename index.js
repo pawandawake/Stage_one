2685fcd901fe613ccbca35185a09e5427627155b
pawandawake <80096040+pawandawake@users.noreply.github.com>
2023-09-11 00:25:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,26 @@ app.get('/', (req, res) => {
     res.send('Express JS on Vercel')
 })
 
-app.get('/ping', (req, res) => {
-    res.send('pong 🏓')
-})
+app.get('/api/', (req, res) => {
+  const slack_name = req.query.slack_name;
+  const track = req.query.track;
+
+  const currentDate = new Date();
+  const currentDay = currentDate.getDay();
+  const time= currentDate.toISOString()
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const currentDayName = daysOfWeek[currentDay];
+  
+res.json({
+  "slack_name": slack_name,
+  "current_day": currentDayName,
+  "utc_time": time,
+  "track": track,
+  "github_file_url": "https://github.com/pawandawake/Stage1/blob/main/server.js",
+  "github_repo_url": "https://github.com/pawandawake/Stage1",
+  "status_code": 200 
+  });
+});
 
 const port = process.env.PORT || 8080
 
